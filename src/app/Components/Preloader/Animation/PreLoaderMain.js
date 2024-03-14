0a696bf8d44c38342ffbcd3e68bed8e2919c1431
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-03-15 00:27:17 +0530

    Preloader Setup

--- src/app/Components/Preloader/Animation/PreLoaderMain.js
+++ src/app/Components/Preloader/Animation/PreLoaderMain.js
@@ -3,7 +3,9 @@ import styles from "./PreLoaderMain.module.css";
 const PreLoaderMain=()=> {
   return (
     <div className={styles.animation}>
-      <h1>Loading...</h1>
+      <div className={styles.loader}>
+        {/* <h1>H</h1> */}
+      </div>
     </div>
   )
 }
