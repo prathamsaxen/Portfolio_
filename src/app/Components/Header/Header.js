8fe10163001be1264e77cb6f7c578da79aefb08d
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-03-20 10:38:19 +0530

    Animation Improved!

--- src/app/Components/Header/Header.js
+++ src/app/Components/Header/Header.js
@@ -1,13 +1,36 @@
+"use client"
 import React from "react";
 import styles from "./Header.module.css";
+import { useLayoutEffect } from "react";
+import gsap from "gsap";
 function Header(props) {
+  useLayoutEffect(()=>{
+    gsap.fromTo(`.heading`, {
+      x: -100,
+      opacity: 0
+  }, {
+      x: 0,
+      opacity: 1,
+      duration: 0.6,
+      stagger: 0.2
+  })
+  gsap.fromTo(`.subHeading`, {
+    opacity: 0,
+    y:20
+}, {
+    opacity: 1,
+    duration: .6,
+    stagger: 0.2,
+    y:0
+})
+  },[])
   return (
     <div className={styles.Header}>
       <div className={styles.logo}>℘</div>
       <div className={styles.HeaderWrapper}>
         <div className={styles.Wrapper}>
-          <h1>{props.title}</h1>
-          <p>{props.subtitle}</p>
+          <h1 className={"heading"}>{props.title}</h1>
+          <p className={"subHeading"}>{props.subtitle}</p>
         </div>
       </div>
       <div className={styles.line}></div>
