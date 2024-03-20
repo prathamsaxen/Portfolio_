b814a3c018affe52704910b0dbd57ea407b88248
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-03-20 14:24:45 +0530

    Preloader fixed!

--- src/app/Components/Preloader/Animation/PreLoaderMain.js
+++ src/app/Components/Preloader/Animation/PreLoaderMain.js
@@ -3,12 +3,11 @@ import styles from "./PreLoaderMain.module.css";
 const PreLoaderMain = () => {
   return (
     <div className={styles.animation}>
-      <div class={styles.typewriter}>
-        <div class={styles.slide}>
-          <i></i>
-        </div>
-        <div class={styles.paper}></div>
-        <div class={styles.keyboard}></div>
+      <div class={styles.content}>
+        <div class={styles.circle}></div>
+        <div class={styles.circle}></div>
+        <div class={styles.circle}></div>
+        <div class={styles.circle}></div>
       </div>
     </div>
   );
