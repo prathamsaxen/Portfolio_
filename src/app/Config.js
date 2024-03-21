7a7af25dfa2306ef8b0c46c379dffb92da827217
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-03-21 19:48:50 +0530

    Fixed Background!

--- src/app/Config.js
+++ src/app/Config.js
@@ -5,6 +5,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import MainPreLoader from "./Components/MainPreLoader/MainPreLoader";
 import Footer from "./Components/Footer/Footer";
 import { useState,useEffect } from "react";
+import styles from "./Config.module.css";
 function Config({ children }) {
     const [loader,setLoader]=useState(true);
     useEffect(()=>{
@@ -18,12 +19,12 @@ function Config({ children }) {
         {loader ? (
           <MainPreLoader />
         ) : (
-          <>
+          <div className={styles.mainWrapper}>
             <ToastContainer />
             <Navbar />
             {children}
             <Footer />
-          </>
+          </div>
         )}
       </>
     </div>
